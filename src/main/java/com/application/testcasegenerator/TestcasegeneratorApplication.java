b5e08613e9dc42aef186f40815c40c857499c6e6
Rohan <rohanmadan1897@gmail.com>
2024-09-02 17:35:12 +0530

    first commit

---
+++ src/main/java/com/application/testcasegenerator/TestcasegeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.application.testcasegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcasegeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcasegeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/testcasegenerator/TestcasegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.testcasegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcasegeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
